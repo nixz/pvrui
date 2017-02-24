e83689ed14cb0743ec9335d67113b23e1d887480
Nikhil Shetty <nikhil.j.shetty@gmail.com>
2017-02-24 13:50:33 -0700

    Making the connection class
    This class is used to make a conneciton to paraview session

--- Connection.cpp
+++ Connection.cpp
@@ -1,7 +1,22 @@
 #include "Connection.h"
 
-Connection::Connection()
-{
+#include <vtkProcessModule.h>
 
+Connection::Connection(std::string host, std::string port)
+{
+      std::stringstream ss;
+      ss <<"cs://"<<host<<":"<<port;
+      this->_session = vtkSMSessionClient::New();
+      this->_session->Connect(ss.str().c_str());
+      this->_id= vtkProcessModule::GetProcessModule()->RegisterSession(this->_session);
 }
 
+Connection::~Connection()
+{
+    this->_session->Delete();
+}
+
+vtkSMSessionClient* Connection::session()
+{
+    return this->_session;
+}
--- Connection.h
+++ Connection.h
@@ -1,11 +1,21 @@
 #ifndef CONNECTION_H
 #define CONNECTION_H
 
+#include <vtkSMSessionClient.h>
+#include <iostream>
+#include <sstream>
+
 
 class Connection
 {
 public:
-    Connection();
+    Connection(std::string host, std::string port);
+    virtual ~Connection();
+    vtkSMSessionClient* session();
+
+private:
+    vtkSMSessionClient * _session;
+    vtkIdType _id;
 };
 
 #endif // CONNECTION_H
